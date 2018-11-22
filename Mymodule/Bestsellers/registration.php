e2fb746fb661953bc92e018f51d9095b0ad578b7
Subrata Bauri <subratabauri987@gmail.com>
2018-11-22 17:00:26 +0530

    Create Bestsellers module

---
+++ Mymodule/Bestsellers/registration.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * @author Subrata Bauri
+ * @copyright Copyright (c) 2018 Subrata Bauri
+ * @package Mymodule_Bestsellers
+ */
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'Mymodule_Bestsellers',
+    __DIR__
+);
