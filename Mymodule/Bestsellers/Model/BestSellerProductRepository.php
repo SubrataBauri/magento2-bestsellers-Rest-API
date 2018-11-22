573a3d99aeac70dbcab2b06fe97cacb5bc305f73
Subrata Bauri <subratabauri987@gmail.com>
2018-11-22 21:43:06 +0530

    add bestsellers to the collection

--- Mymodule/Bestsellers/Model/BestSellerProductRepository.php
+++ Mymodule/Bestsellers/Model/BestSellerProductRepository.php
@@ -32,8 +32,14 @@ class BestSellerProductRepository implements \Mymodule\Bestsellers\Api\BestSelle
      */
     private $searchResultsFactory;
 
+    /**
+     * @var \Magento\Sales\Model\ResourceModel\Report\Bestsellers\CollectionFactory
+     */
+    private $bestSellersCollectionFactory;
+
     public function __construct(
         \Magento\Catalog\Model\ResourceModel\Product\CollectionFactory $collectionFactory,
+        \Magento\Sales\Model\ResourceModel\Report\Bestsellers\CollectionFactory $bestSellersCollectionFactory,
         \Magento\Framework\Api\ExtensionAttribute\JoinProcessorInterface $extensionAttributesJoinProcessor,
         \Magento\Framework\Api\SearchCriteria\CollectionProcessorInterface $collectionProcessor,
         \Magento\Catalog\Api\Data\ProductSearchResultsInterfaceFactory $searchResultsFactory
@@ -42,6 +48,7 @@ class BestSellerProductRepository implements \Mymodule\Bestsellers\Api\BestSelle
         $this->extensionAttributesJoinProcessor = $extensionAttributesJoinProcessor;
         $this->collectionProcessor = $collectionProcessor;
         $this->searchResultsFactory = $searchResultsFactory;
+        $this->bestSellersCollectionFactory = $bestSellersCollectionFactory;
     }
     
     /**
@@ -49,17 +56,31 @@ class BestSellerProductRepository implements \Mymodule\Bestsellers\Api\BestSelle
      */
     public function getList($fromDate, $toDate, \Magento\Framework\Api\SearchCriteriaInterface $searchCriteria)
     {
+        $productIds = [];
+        $pageSize = ($searchCriteria->getPageSize() === null) ? $searchCriteria->getPageSize(): $this->defaultPageSize;
+        
+        $bestSellersCollection = $this->bestSellersCollectionFactory->create();
+        $bestSellersCollection->setModel(\Magento\Catalog\Model\Product::class);
+        $bestSellersCollection->setPeriod('year')->setDateRange($fromDate, $toDate);
+        $bestSellersCollection->setPageSize($pageSize)->setCurPage(1);
+        $bestSellersCollection->load();
+        
+        $productIds = $bestSellersCollection->getColumnValues('product_id');
+
         $collection = $this->collectionFactory->create();
         $this->extensionAttributesJoinProcessor->process($collection);
         $collection->addAttributeToSelect('*');
+        $collection->addIdFilter($productIds);
         $collection->joinAttribute('status', 'catalog_product/status', 'entity_id', null, 'inner');
         $collection->joinAttribute('visibility', 'catalog_product/visibility', 'entity_id', null, 'inner');
+        
         $this->collectionProcessor->process($searchCriteria,$collection);
         $collection->load();
+        
         $searchResult = $this->searchResultsFactory->create();
         $searchResult->setSearchCriteria($searchCriteria);
         $searchResult->setItems($collection->getItems());
-        $searchResult->setTotalCount($collection->getSize());
+        $searchResult->setTotalCount($collection->getSize());       
         return $searchResult;
     }
 }
